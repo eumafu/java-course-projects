ea049a7bc8d954c3168dd9a7ba1baeb79b2b3b99
Henrique Almeida <pedrotcgt232@gmail.com>
2025-04-18 17:14:57 -0300

    Organização geral do repositório

--- JavaNelioAlves/src/01 - Firt Program in Java/AprendendoPrint2.java
+++
@@ -1,9 +0,0 @@
-public class AprendendoPrint2 {
-    public static void main(String[] args) {
-        String nome = "Lolozinha";
-        int idade = 18;
-        Double renda = 2000.00;
-        System.out.printf("%s tem %d anos e ganha R$%.2f reais %n", nome, idade, renda);
-
-    }
-    }
