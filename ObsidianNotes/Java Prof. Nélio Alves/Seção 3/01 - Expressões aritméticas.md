9fb5861eca5d792638e1709675be509051af5068
Henrique Almeida <pedrotcgt232@gmail.com>
2025-04-19 17:33:38 -0300

    teste

--- ObsidianNotes/Java Prof. Nélio Alves/Seção 3/01 - Expressões aritméticas.md
+++
@@ -1,6 +0,0 @@
-
----
-![[Pasted image 20250414210958.png]]
-
-![[Pasted image 20250414211358.png]]
-
--- java-nelio-alves/src/firt_program_java/HelloWord.java
+++
@@ -1,7 +0,0 @@
-package firt_program_java;
-
-public class HelloWord {
-    public static void main(String[] args) {
-        System.out.println("Hello Word!");
-    }
-}
